d60bb3465baaddd00cce3440cf666be656e26fa0
Jyotsna Gupta <jyotsna17gupta@gmail.com>
2020-06-30 06:01:06 +0530

    add fibonacci and its tests

---
+++ internals/fibonacci.go
@@ -0,0 +1,12 @@
+package internals
+
+import "math"
+
+func IsFibonacci(n int) bool {
+	return isPerfectSquare(5*n*n+4) || isPerfectSquare(5*n*n-4)
+}
+
+func isPerfectSquare(n int) bool {
+	sqrt := int(math.Sqrt(float64(n)))
+	return (sqrt * sqrt) == n
+}
---
+++ internals/fibonacci_test.go
@@ -0,0 +1,85 @@
+package internals
+
+import "testing"
+
+func TestIsFibonacci(t *testing.T) {
+	tests := []struct {
+		name string
+		n    int
+		want bool
+	}{
+		{
+			name: "when number is 8",
+			n:    8,
+			want: true,
+		},
+		{
+			name: "when number is 1",
+			n:    1,
+			want: true,
+		},
+		{
+			name: "when number is 16",
+			n:    16,
+			want: false,
+		},
+		{
+			name: "when number is 4",
+			n:    4,
+			want: false,
+		},
+		{
+			name: "when number is 22",
+			n:    22,
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsFibonacci(tt.n); got != tt.want {
+				t.Errorf("IsFibonacci() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_isPerfectSquare(t *testing.T) {
+	tests := []struct {
+		name string
+		n    int
+		want bool
+	}{
+		{
+			name: "when number is 25",
+			n:    25,
+			want: true,
+		},
+		{
+			name: "when number is 21",
+			n:    21,
+			want: false,
+		},
+		{
+			name: "when number is 15",
+			n:    15,
+			want: false,
+		},
+		{
+			name: "when number is 4",
+			n:    4,
+			want: true,
+		},
+		{
+			name: "when number is 100",
+			n:    100,
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isPerfectSquare(tt.n); got != tt.want {
+				t.Errorf("isPerfectSquare() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
